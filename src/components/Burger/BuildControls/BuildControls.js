f85f8ad473fecba97c2f907a953dcb646af9dfbb
KennethOlsen <kennetholsen87@gmail.com>
2019-12-05 15:09:10 +0100

    feat: className to Order-Now button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,7 +22,7 @@ const buildControls = (props) => (
             key={ctrl.label} 
             label={ctrl.label}/>
         ))}
-        <button>ORDER NOW</button>
+        <button className={classes.OrderButton}>ORDER NOW</button>
     </div>
 )
 
