41e5dfc0f6ceb73b2aa5e7450920905fc6b22635
KennethOlsen <kennetholsen87@gmail.com>
2019-12-06 14:19:29 +0100

    perf: adding lifecycle-hook to Modal. Only rendering when updated

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,6 +6,15 @@ import Backdrop from '../Backdrop/Backdrop'
 
 
 class Modal extends Component {
+
+    shouldComponentUpdate( nextProps, nextState ) {
+         return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal] DidUpdate')
+    }
+
     render() {
         return (
         <Aux>
@@ -15,8 +24,7 @@ class Modal extends Component {
                 style={{
                     transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
                     opacity: this.props.show ? '1' : '0'
-                }}
-                >
+                }}>
                 {this.props.children}
             </div>
         </Aux>
