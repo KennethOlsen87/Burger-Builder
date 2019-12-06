c6a325fd38e53a000c7cf6c25fe0e607b80a954a
KennethOlsen <kennetholsen87@gmail.com>
2019-12-06 08:29:02 +0100

    feat: added price onto Summary-Modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -99,6 +99,7 @@ class BurgerBuilder extends Component {
                 >
                     <OrderSummary
                         ingredients={this.state.ingredients} 
+                        price={this.state.totalPrice}
                         purchaseCancelled={this.purchaseCancelHandler}
                         purchaseContiniued={this.purchaseContiniueHandler}
                     />
