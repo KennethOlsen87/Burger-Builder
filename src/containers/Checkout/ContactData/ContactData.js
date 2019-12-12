8b1e9a47d00f418e8eaba2e9c5f43f9fb4ab3376
KennethOlsen <kennetholsen87@gmail.com>
2019-12-12 11:36:00 +0100

    fix: minor edit to fix a little error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -48,10 +48,10 @@ class ContactData extends Component {
     render () {
         let form = (
             <form>
-                <Input inputType="input" type='text' name='name' placeholder='Your Name' />
-                <Input inputType="input" type='email' name='email' placeholder='Your Email' />
-                <Input inputType="input" type='text' name='street' placeholder='Street' />
-                <Input inputType="input" type='text' name='postal' placeholder='Postal Code' />
+                <Input inputtype="input" type='text' name='name' placeholder='Your Name' />
+                <Input inputtype="input" type='email' name='email' placeholder='Your Email' />
+                <Input inputtype="input" type='text' name='street' placeholder='Street' />
+                <Input inputtype="input" type='text' name='postal' placeholder='Postal Code' />
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         )
