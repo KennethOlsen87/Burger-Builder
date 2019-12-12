e7b6593a16c710380fe320e45c10d2f52131bcc8
KennethOlsen <kennetholsen87@gmail.com>
2019-12-12 12:13:50 +0100

    feat: created a simple dropdown

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -8,13 +8,29 @@ const input = (props) => {
     switch (props.elementType) {
         case('input'):
             inputElement = <input 
-                className={classes.InputElement} {...props.elementConfig} 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
                 value={props.value} />
             break
         case ('textarea'): 
             inputElement = <textarea 
-                className={classes.InputElement} {...props.elementConfig} 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
                 value={props.value}/>
+            break
+             case ('select'): 
+            inputElement = (
+                <select 
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}> 
+                            {option.displayValue}
+                        </option>
+                    )
+                    )}
+                </select>
+                )
             break
         default: 
             inputElement = <input 
