58a911ad39f43e2bd235160b87fcdfee166ee297
KennethOlsen <kennetholsen87@gmail.com>
2019-12-16 13:47:09 +0100

    feat: created a simple start for reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actions'
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 50
+
+}
+
+const reducer = (state = initialState, action) => {
+
+
+}
+
+export default reducer
