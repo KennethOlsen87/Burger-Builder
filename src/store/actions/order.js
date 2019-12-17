56f3f7aa3d9482064c49ae39ec045e6474c15b01
KennethOlsen <kennetholsen87@gmail.com>
2019-12-17 13:45:27 +0100

    feat: creating async action for purchaseBurgerStart using axios

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes'
+import axios from '../../axios-orders'
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
@@ -12,5 +13,18 @@ export const purchaseBurgerFail = (error) => {
     return {
         type: actionTypes.PURCHASE_BURGER_FAIL,
         error: error
+    }
+}
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post( '/orders.json', orderData )
+            .then( response => {
+                dispatch(purchaseBurgerSuccess(response.data))
+                console.log(response.data, orderData)
+            } )
+            .catch( error => {
+                dispatch(purchaseBurgerFail(error))
+            } );
     }
 }
