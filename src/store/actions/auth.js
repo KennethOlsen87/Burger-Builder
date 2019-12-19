4cc8a3e15eaf62b1153fb704fcea947c8f2f5e53
KennethOlsen <kennetholsen87@gmail.com>
2019-12-19 09:26:48 +0100

    feat: getting the token and id in authSuccess

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     }
 }
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token, 
+        userId: userId
     }
 }
 
@@ -33,7 +34,7 @@ export const auth = (email, password) => {
         axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCH2TWQ104bkC8YknXh9Nesygl5bPbi670', authData)
         .then(response => {
             console.log(response)
-            dispatch(authSuccess(response.data))
+            dispatch(authSuccess(response.data.idToken, response.data.localId))
         })
             
         .catch(err => {
