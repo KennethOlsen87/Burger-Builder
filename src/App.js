e536aebcbf05b11d6872121af3d48b9062f25bea
KennethOlsen <kennetholsen87@gmail.com>
2020-01-06 08:47:29 +0100

    feat: converting <App>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import asyncComponent from './hoc/asyncComponent/asyncComponent'
@@ -18,14 +18,13 @@ const asyncAuth = asyncComponent(() => {
   return import('./containers/Auth/Auth')
 })
 
-class App extends Component {
+const app = props => {
 
-  componentDidMount() {
-    this.props.onTryAutoSignup()
-  }
+  useEffect(() => {
+    props.onTryAutoSignup()
+  }, [])
 
 
-  render () {
     let routes = (
           <Switch>
             <Route path="/auth" component={asyncAuth} />
@@ -34,7 +33,7 @@ class App extends Component {
           </Switch>
     )
 
-    if (this.props.isAuthenticated) {
+    if (props.isAuthenticated) {
       routes = (
          <Switch>
             <Route path="/checkout" component={asyncCheckout} />
@@ -54,7 +53,6 @@ class App extends Component {
         </Layout>
       </div>
     );
-  }
 }
 
 const mapStateToProps = state => {
@@ -71,4 +69,4 @@ const mapDispatchToProps = dispatch => {
 
 
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app))
